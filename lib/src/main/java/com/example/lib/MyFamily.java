5214b0864d97701c9034506290445ae31c4a3b50
DennsHartstock <dennis.hartstock@web.de>
2022-11-03 16:18:09 +0100

    add HelloWorld.java and MyFamily.java

---
+++ lib/src/main/java/com/example/lib/MyFamily.java
@@ -0,0 +1,11 @@
+package com.example.lib;
+
+public class MyFamily {
+
+    public static void main(String[] args) {
+
+        System.out.println("Vita");
+        System.out.println("Lilli");
+        System.out.println("Dennis");
+    }
+}
