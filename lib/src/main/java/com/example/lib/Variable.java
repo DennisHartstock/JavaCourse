260d26fe85d2718d4e7f711dae7e532ccf420408
DennsHartstock <dennis.hartstock@web.de>
2022-11-04 06:57:19 +0100

    add Variable.java

---
+++ lib/src/main/java/com/example/lib/Variable.java
@@ -0,0 +1,27 @@
+package com.example.lib;
+
+public class Variable {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = x + 5;
+        System.out.println(y);
+
+        int dollarsHasJohn = 100;
+        int dollarsHasNick = dollarsHasJohn * 5;
+        int dollarsSum = dollarsHasJohn + dollarsHasNick;
+        System.out.println(dollarsSum);
+
+        int a = 10;
+        int b = 3;
+        int c = a % b;
+        System.out.println(c);
+
+        int days = 10000;
+        int years = days / 365;
+        int months = (days % 365) / 30;
+        int leftDays = (days % 365) % 30;
+        System.out.println(years + " " + months + " " + leftDays);
+    }
+}
