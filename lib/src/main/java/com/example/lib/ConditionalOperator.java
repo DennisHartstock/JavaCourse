8cc280cfbb4ffcf173a43d04165d8f9f3d8d4d5a
DennsHartstock <dennis.hartstock@web.de>
2022-11-06 10:13:03 +0100

    Boolean and conditional operators

--- lib/src/main/java/com/example/lib/ConditionalOperator.java
+++ lib/src/main/java/com/example/lib/ConditionalOperator.java
@@ -3,12 +3,15 @@ package com.example.lib;
 public class ConditionalOperator {
 
     public static void main(String[] args) {
+        int temp = 30;
+        boolean isHot = temp >= 25;
+        boolean isCold = temp <= 22;
+        int time = 23;
+        boolean isNight = time > 22 || time < 6;
 
-        int temp = 23;
-
-        if (temp > 25) {
+        if (isHot && !isNight) {
             System.out.println("Air Conditioner is ON");
-        } else if (temp < 22) {
+        } else if (isCold) {
             System.out.println("Air Conditioner is OFF");
         } else {
             System.out.println("Room temperature is optimal");
---
+++ lib/src/main/java/com/example/lib/WhatToDo.java
@@ -0,0 +1,30 @@
+package com.example.lib;
+
+public class WhatToDo {
+
+    public static void main(String[] args) {
+        int time = 23;
+        boolean isNight = time > 22 || time < 6;
+        boolean isGoodWeather = false;
+
+        if (time > 6 && time < 10 || time > 14 && time < 18) {
+            System.out.println("Learn");
+        }
+
+        if (time > 10 && time < 12) {
+            if (isGoodWeather)
+                System.out.println("Go jogging");
+            if (!isGoodWeather) {
+                System.out.println("Go to the gym");
+            }
+        }
+
+        if (!isNight) {
+            System.out.println("Read books");
+        }
+
+        if (isNight) {
+            System.out.println("Sleep");
+        }
+    }
+}
