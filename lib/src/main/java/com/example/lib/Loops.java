96c2e40869142ffb886603f3bd6b29b148d0e27f
DennsHartstock <dennis.hartstock@web.de>
2022-11-07 06:26:37 +0100

    Loops

---
+++ lib/src/main/java/com/example/lib/Loops.java
@@ -0,0 +1,29 @@
+package com.example.lib;
+
+public class Loops {
+
+    public static void main(String[] args) {
+//        int i = 0;
+//        while (i <= 1000) {
+//            if (i % 2 == 0) {
+//                System.out.println(i);
+//            }
+//            i++;
+//        }
+
+//        int a = 0;
+//        do {
+//            System.out.println("Hello");
+//        } while (a > 0);
+
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 1000; i >= 0; i--) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
