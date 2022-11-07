ca0b98dd02f1b9b95327541c7d38028c9a011413
DennsHartstock <dennis.hartstock@web.de>
2022-11-07 05:56:43 +0100

    Strings

---
+++ lib/src/main/java/com/example/lib/Strings.java
@@ -0,0 +1,15 @@
+package com.example.lib;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "World";
+        System.out.println(hello + " " + world + "!");
+
+        String name = "name";
+        int age = 13;
+        String result = "Hello \"" + name + "\"!\nYour are " + age + " years old.";
+        System.out.println(result);
+    }
+}
