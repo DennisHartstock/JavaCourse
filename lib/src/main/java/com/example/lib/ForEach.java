bc28e499232a294bfe8affb14cb68f0fa09fa85b
DennsHartstock <dennis.hartstock@web.de>
2022-11-08 09:15:54 +0100

    for each

---
+++ lib/src/main/java/com/example/lib/ForEach.java
@@ -0,0 +1,26 @@
+package com.example.lib;
+
+public class ForEach {
+
+    public static void main(String[] args) {
+        String[] names = {
+                "Vita",
+                "Lilli",
+                "Dennis"
+        };
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        int[] numbers = new int[100];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = 100 + i;
+        }
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
