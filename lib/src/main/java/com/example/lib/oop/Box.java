8ae5e81ba328de42088b3e37a2c93c5177195a73
DennsHartstock <dennis.hartstock@web.de>
2022-11-09 16:54:15 +0100

    reference types in methods and constructors

--- lib/src/main/java/com/example/lib/oop/Box.java
+++ lib/src/main/java/com/example/lib/oop/Box.java
@@ -9,6 +9,10 @@ public class Box {
         this(10);
     }
 
+    Box(Box another) {
+        this(another.length, another.width, another.height);
+    }
+
     Box(double length, double width, double height) {
         this.length = length;
         this.width = width;
@@ -23,6 +27,29 @@ public class Box {
         this.length = length;
         this.width = width;
         this.height = height;
+    }
+
+    Box copy() {
+        return new Box(length, width, height);
+    }
+
+    Box increase() {
+        return new Box(length * 2, width * 2, height * 2);
+    }
+
+    int compare(Box another) {
+        double currentVolume = getVolume();
+        double anotherVolume = another.getVolume();
+        int result;
+
+        if (currentVolume > anotherVolume) {
+            result = 1;
+        } else if (currentVolume < anotherVolume) {
+            result = -1;
+        } else {
+            result = 0;
+        }
+        return result;
     }
 
     double getVolume() {
--- lib/src/main/java/com/example/lib/oop/Main.java
+++ lib/src/main/java/com/example/lib/oop/Main.java
@@ -3,8 +3,9 @@ package com.example.lib.oop;
 public class Main {
 
     public static void main(String[] args) {
-        Monster monster = new Monster(4, 8, 4);
-        System.out.println(monster.arms);
-        monster.voice(3, "Aaaaaaa....");
+        Box current = new Box(10);
+        Box another = current.increase();
+        current.showVolume();
+        another.showVolume();
     }
 }
