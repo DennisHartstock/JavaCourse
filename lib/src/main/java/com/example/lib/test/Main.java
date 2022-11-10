b1704f3a544dba06782454340a711603e95b188f
DennsHartstock <dennis.hartstock@web.de>
2022-11-10 08:59:23 +0100

    static, final and varargs

--- lib/src/main/java/com/example/lib/test/Main.java
+++ lib/src/main/java/com/example/lib/test/Main.java
@@ -1,11 +1,12 @@
 package com.example.lib.test;
 
-import com.example.lib.oop.Box;
+import com.example.lib.oop.MyMath;
 
 public class Main {
 
     public static void main(String[] args) {
-        Box box = new Box(5, 4, 6);
-        box.showVolume();
+        System.out.println(MyMath.length(3));
+        System.out.println(MyMath.area(4));
+        System.out.println(MyMath.sum(4, 5, 1));
     }
 }
