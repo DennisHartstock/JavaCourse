909b3b6de6cf5c79b5a06ceaa38f3333aa187d08
DennsHartstock <dennis.hartstock@web.de>
2022-11-11 06:07:24 +0100

    ArrayList and HashSet

--- lib/src/main/java/com/example/lib/test/Main.java
+++ lib/src/main/java/com/example/lib/test/Main.java
@@ -1,21 +1,64 @@
 package com.example.lib.test;
 
-import com.example.lib.oop.Person;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
-//        String s = "10";
-//        String s1 = "20";
-//        int x = Integer.parseInt(s);
-//        int y = Integer.parseInt(s1);
+        HashSet<String> namesHashSet = new HashSet<>();
+        namesHashSet.add("John");
+        namesHashSet.add("Emma");
+        namesHashSet.add("Alice");
+        namesHashSet.add("Alice");
+        namesHashSet.add("Tom");
+        namesHashSet.add("Mickey");
+        for (String name : namesHashSet) {
+            System.out.println(name);
+        }
 
-//        System.out.println(x + y);
+//        ArrayList<String> names = new ArrayList<>();
+//        names.add("Vita");
+//        names.add("Lilli");
+//        names.add("Dennis");
+//
+//        ArrayList<Integer> numbers = new ArrayList<>();
+//        numbers.add(1);
+//        numbers.add(2);
+//        numbers.add(3);
+//
+//        ArrayList<String> personList = new ArrayList<>();
+//        for (int i = 0; i < numbers.size(); i++) {
+//            personList.add("Number: " + numbers.get(i) + "     Person: " + names.get(i));
+//        }
+//        for (String person : personList) {
+//            System.out.println(person);
+//        }
 
-        String s = "This is John. He is 27 years old.";
-        String name = s.substring(8, 12);
-        int age = Integer.parseInt(s.substring(20, 22));
-        Person person = new Person(name, age);
-        System.out.println(person.getName() + "\n" + person.getAge());
+
+//        ArrayList<String> employees = getEmployees();
+//        employees.add("Emma");
+//        employees.remove("Donny");
+//        employees.remove(0);
+//        for (String employee : employees) {
+//            System.out.println(employee);
+//        }
+
+//        ArrayList<Integer> numbers = new ArrayList<>();
+//        for (int i = 0; i < 1000; i++) {
+//            numbers.add(i);
+//        }
+//        for (int number : numbers) {
+//            System.out.println(number);
+//        }
     }
+//
+//    private static ArrayList<String> getEmployees() {
+//        ArrayList<String> employees = new ArrayList<>();
+//        employees.add("Nick");
+//        employees.add("John");
+//        employees.add("Richard");
+//        employees.add("Michael");
+//        employees.add("Donny");
+//        return employees;
+//    }
 }
